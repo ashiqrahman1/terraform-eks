43ed44e0ed59f095c6ccbdfc6ca4ad92477a0255
ashiq <Ashiq Rahman>
2023-09-20 23:06:16 +0530

    02-terraform-vpc

---
+++ 02-Terraform-VPC/variables.tf
@@ -0,0 +1,15 @@
+variable region {
+  type        = string
+  default     = "us-east-1"
+  description = "description"
+}
+
+variable "env" {
+  type = string
+  default = ""
+}
+
+variable "department" {
+  type = string
+  default = ""
+}
